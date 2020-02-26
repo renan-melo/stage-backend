3141b0c08afed371a6aa7b94cf24a6df265b6f49
renan-melo <git@seehub.com.brm>
2020-02-25 23:53:00 -0300

    Ajuste telas

--- backend/src/templates/sql/getBanner.sql
+++ backend/src/templates/sql/getBanner.sql
@@ -1,6 +1,6 @@
-select ti.img_banner,tp.title from tab_img ti
-{{#id_group}}
+select ti.img_banner as src,tp.title from tab_img ti
 inner join tab_post tp on ti.id_post = tp.id
+{{#id_group}}
 inner join tab_category tc on tp.id_category = tc.id
 inner join tab_group tg on tc.id_group = tg.id
 where tg.id = :id_group
